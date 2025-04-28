82231f5e05cd5a10946047e28db9e19db1d5b867
unknown <RahimUllah001>
2025-04-28 07:43:44 -0700

    project 13

---
+++ gs13/gs13/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path,include
+from api import views
+from rest_framework.routers import DefaultRouter
+
+from rest_framework.authtoken.views import obtain_auth_token
+
+# Creating Router object
+router = DefaultRouter()
+
+# Register student viewset mean class with defalt router we are dioing this so that we habve not to maully accord url to every end point
+
+# router.register('studentapi',views.StudentViewSet,basename='student')     #use this if want to use more code or use below for two lines
+
+router.register('studentapi',views.StudentModelviewSet,basename='student')
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include(router.urls)),
+    path("auth/",include('rest_framework.urls')),
+    path("gettoken/",obtain_auth_token)
+]
